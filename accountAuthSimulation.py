bb13899e678292556b01a82eb08de8c107d7463e
Rique <riquecatipay@gmail.com>
2025-03-18 23:25:33 +0800

    modified another way

--- accountAuthSimulation.py
+++ accountAuthSimulation.py
@@ -5,29 +5,35 @@ passwords = ["francesca11", "toosweet12", "sedated2001"]
 usernameField = input("Enter your registered username: ")
 passwordField = input('Enter your password: ')
 
-for username in usernames:
-   if username == usernameField:
-      usernameMatch = username 
-      usernameIndex = usernames.index(username)
+# for username in usernames:
+#    if username == usernameField:
+#       usernameMatch = username 
+#       usernameIndex = usernames.index(username)
    
      
 
-      for password in passwords:
-        if password == passwordField:
-           passwordMatch = password
-           passwordIndex = passwords.index(password)
+#       for password in passwords:
+#         if password == passwordField:
+#            passwordMatch = password
+#            passwordIndex = passwords.index(password)
         
            
 
-           if usernameIndex == passwordIndex:
-              print("Welcome " + username)
+#            if usernameIndex == passwordIndex:
+#               print("Welcome " + username)
             
-           else: 
-              print("Account not Found")
+#            else: 
+#               print("Account not Found")
            
            
 
-           
+for x in range(len(usernames)):
+    if usernameField == usernames[x] and passwordField == passwords[x]:
+        print("Welcome " + usernames[x])
+        break
+else: 
+    print("Account not Found")
+
 
 
                 
