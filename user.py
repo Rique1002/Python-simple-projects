78c885f4a1b06ee1b5288498f9827f1a583baa05
Rique <riquecatipay@gmail.com>
2025-04-16 16:22:06 +0800

    Added user objfunction

---
+++ user.py
@@ -0,0 +1,26 @@
+class user:
+    def __init__(self,firstName,lastName,likesCount, *friendsName):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.likesCount = likesCount
+        self.friendsName = friendsName
+
+    def introduce(self):
+        print("Hi I am " + self.firstName + " "+ self.lastName)
+
+    def fullProfile(self):
+        print("Full Name: " + self.firstName + " " + self.lastName)
+        print("Likes: " + str(self.likesCount))
+        print("Friends")
+        
+        for friend in self.friendsName:
+            print(" -" + friend )
+            
+
+
+
+userA = user("David", "SDPT", 10, "Alenere Sdpt", "Jaymar Catapang", "Joshua Emmanuel Seria", "Patrick Macaraig")  
+userA.introduce()
+userA.fullProfile()
+
+     
